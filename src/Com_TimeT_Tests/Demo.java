184c95e83c5ab434a1d4f0a189885fce79598103
Maddy <Maddy@Lenovo-PC>
2020-12-31 14:52:54 +0530

    version 1.1

---
+++ src/Com_TimeT_Tests/Demo.java
@@ -0,0 +1,14 @@
+package Com_TimeT_Tests;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo 
+{
+	@Test
+	public void demo()
+	{
+		Reporter.log("hiiii", true);
+	}
+
+}
